2d06072dc592c48a6d9b14a3d9d1bfeb0be7e48d
Caleb Eby <caleb.eby01@gmail.com>
2018-02-11 10:59:52 -0800

    Correctly implement electron build

--- build.js
+++ build.js
@@ -5,6 +5,10 @@ const build = env => {
   const webpackConfig = require('./webpack.config')(env)
   const compiler = webpack(webpackConfig)
   compiler.run(printStats)
+  if (env.electron) {
+    const mainCompiler = webpack(webpackConfig.electron)
+    mainCompiler.run(printStats)
+  }
 }
 
 module.exports = build
